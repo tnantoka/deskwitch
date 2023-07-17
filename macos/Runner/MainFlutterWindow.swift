62de0db28c6b7cca2065fede19240fda4e50d07b
tnantoka <tnantoka@bornneet.com>
2023-07-17 10:08:34 +0900

    feat: show at bottom right

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,10 +1,27 @@
 import Cocoa
 import FlutterMacOS
 
+public extension NSView {
+  var mouseDownCanMoveWindow: Bool { true }
+}
+
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+
+    flutterViewController.backgroundColor = .clear
+    self.backgroundColor = .clear
+    self.styleMask = [.borderless]
+    self.level = .floating
+    let visibleFrame = NSScreen.main!.visibleFrame
+    let gameSize: CGFloat = 100;
+    let windowFrame = NSMakeRect(
+      visibleFrame.size.width - gameSize,
+      visibleFrame.origin.y,
+      gameSize,
+      gameSize
+    )
+
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
